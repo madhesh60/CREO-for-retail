75777218042d9e113c4655dd6d40c2eb3aea3ccc
madhesh60 <madheshsasikala81@gmail.com>
2026-01-05 16:05:43 +0530

    added features

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 client = AsyncIOMotorClient(MONGO_URL)
